60acc0039a5b4e95f2727026b1353f92b51b9b2d
Ivan Alburquerque <Albur.Ivan@outlook.com>
2017-07-04 15:42:34 -0400

    Added Initial BubbleSort algorithm

---
+++ src/main/java/com/raworkstudio/practice/bubblesort/BubbleSort.kt
@@ -0,0 +1,24 @@
+package com.raworkstudio.practice.bubblesort
+
+import com.raworkstudio.practice.core.Algorithm
+
+/**
+ * Created by Ivan Alburquerque on 7/3/2017.
+ */
+class BubbleSort : Algorithm {
+
+    override fun getName(): String = "Bubble Sort"
+
+    override fun getDescription(): String = "This is bubble sort"
+
+    override fun sort(array: ArrayList<Int>): ArrayList<Int> {
+        val sortedList = ArrayList<Int>()
+
+
+
+        return sortedList
+    }
+
+
+
+}
