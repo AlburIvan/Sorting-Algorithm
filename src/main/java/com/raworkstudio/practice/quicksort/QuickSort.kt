a2462a9f10085df1a247d269f137854bde1fbd7d
Ivan Alburquerque <Albur.Ivan@outlook.com>
2017-07-04 15:43:00 -0400

    Added initial QuickSort template

---
+++ src/main/java/com/raworkstudio/practice/quicksort/QuickSort.kt
@@ -0,0 +1,24 @@
+package com.raworkstudio.practice.quicksort
+
+import com.raworkstudio.practice.core.Algorithm
+
+/**
+ * Created by Ivan Alburquerque on 7/4/2017
+ */
+class QuickSort: Algorithm {
+
+    override fun sort(array: ArrayList<Int>): ArrayList<Int> {
+        val sortedList = java.util.ArrayList<Int>()
+
+
+        
+
+
+
+        return sortedList
+    }
+
+    override fun getDescription(): String = "QuickSort is a divide and conquer algorithm"
+
+    override fun getName(): String = "QuickSort"
+}
